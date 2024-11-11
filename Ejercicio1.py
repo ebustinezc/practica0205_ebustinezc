290920ed60d59208d290e591ccbc6db1710402da
ebustinezc <ebustinezc@educacion.navarra.es>
2024-11-11 10:26:48 +0100

    Ejercicio1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,2 +1,12 @@
 # Escribir una función a la que se le pase una cadena <nombre> 
 # y muestre por pantalla el saludo ¡hola <nombre>!.
+def saludo(nombre):
+    '''Funcion que saluda
+    Parametros:
+        -nombre:Nombre de la persona a saludar
+    salida:
+        saludo con el nombre de la persona'''
+    
+    return('¡Hola {}!'.format(nombre))
+#nombre = input('Dime tu nombre:')
+print(saludo('Bustin'))
