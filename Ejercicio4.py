5db905b7c3b39a3fa00d64fa889cc8f2ed06594e
ebustinezc <ebustinezc@educacion.navarra.es>
2024-11-11 18:15:31 +0100

    Ejercicio4

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,1 +1,15 @@
 # Escribir una función que reciba una muestra de números en una lista y devuelva su media.
+def media(numero):
+    """Función que calcula la media de una serie de números
+    Parámetros:
+        -Lista de Números
+    Salida:
+        Media de todos los números introducidos"""
+    media = 0
+    numero = numero.split()
+    for x in numero:
+        media = media + int(x)
+    media = media / len(numero)
+    return("La media es: {}".format(media))
+numero = input("Dime una serie de números separados por espacios:")
+print(media(numero))
