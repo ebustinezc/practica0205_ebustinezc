f7ec5af52dde9a331b70c3bf2f32c17d94b683ba
ebustinezc <ebustinezc@educacion.navarra.es>
2024-11-11 18:17:54 +0100

    Ejercicio5

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,1 +1,14 @@
 # Escribir una función que reciba una muestra de números en una lista y devuelva otra lista con sus valores al cuadrado.
+def cuadrado(numero):
+    """Función que recibe unos números y los devuelve al cuadrado
+    Parámetros:
+        -n: lista de los números
+    Salida:
+        Lista de los números al cuadrado"""
+    cuadrado = []
+    numero = numero.split()
+    for x in range(len(numero)):
+        cuadrado.append(int(numero[x]) ** 2)
+    return cuadrado
+numero = input("Dime una lista de números separados por espacios:")
+print(cuadrado(numero))
